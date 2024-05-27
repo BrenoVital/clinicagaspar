6c4a60ef44158dc583642887511693a6440dd705
breno vital <brenovital50@gmail.com>
2024-05-27 11:19:27 -0300

    init: refactoring to react,typescript

---
+++ Clinica_Gaspar/src/shared/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Header() {
+  return (
+    <Box>
+      <Box></Box>
+      <Typography variant="h1">Clinica Gaspar</Typography>
+    </Box>
+  );
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # clinicagaspar
+
 Esse projeto foi feito com a finalidade de praticar minhas habilidades em HTML/CSS e mostrar meus atuais projetos finalizados.
 
+# Transformando o projeto de html e css para React/TS
